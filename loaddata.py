a96bbeba39102c2b11ebba39f741606f1802bc65
designer357 <cityumc@hotmail.com>
2017-02-16 22:19:53 +0800

    add undersampling and oversampling methods

--- loaddata.py
+++ loaddata.py
@@ -1,6 +1,25 @@
 import os
 import numpy as np
 import random as rd
+from sklearn.datasets import make_classification
+
+from unbalanced_dataset.unbalanced_dataset import UnbalancedDataset
+from unbalanced_dataset.over_sampling import OverSampler
+from unbalanced_dataset.over_sampling import SMOTE
+
+from unbalanced_dataset.under_sampling import UnderSampler
+from unbalanced_dataset.under_sampling import TomekLinks
+from unbalanced_dataset.under_sampling import ClusterCentroids
+from unbalanced_dataset.under_sampling import NearMiss
+from unbalanced_dataset.under_sampling import CondensedNearestNeighbour
+from unbalanced_dataset.under_sampling import OneSidedSelection
+from unbalanced_dataset.under_sampling import NeighbourhoodCleaningRule
+
+from unbalanced_dataset.ensemble_sampling import EasyEnsemble
+from unbalanced_dataset.ensemble_sampling import BalanceCascade
+
+from unbalanced_dataset.pipeline import SMOTEENN
+from unbalanced_dataset.pipeline import SMOTETomek
 def loadData(input_data_path,filename):
     global positive_sign,negative_sign
     with open(os.path.join(input_data_path,filename)) as fin:
@@ -35,14 +54,14 @@ def cross_tab2(input_data_path,filename_training,filename_testing):
     return data_training[:, :-1], data_training[:, -1], data_testing[:, :-1], data_testing[:, -1]
 
 
-def cross_tab(data,cross_folder,tab_cv):
+def cross_tab(data,cross_folder,tab_cv, flag):
     posi_data=data[data[:,-1]!=1.0]
     nega_data=data[data[:,-1]==1.0]
     #print("Positive is "+str(len(posi_data)))
     #print("Negative is "+str(len(nega_data)))
 
-    print("IR is :"+str(float(len(nega_data))/len(posi_data)))
-    print("The anomalies is "+str(len(posi_data)))
+    #print("IR is :"+str(float(len(nega_data))/len(posi_data)))
+    #print("The anomalies is "+str(len(posi_data)))
     p_indexes=[i for i in range(len(posi_data))]
     n_indexes=[i for i in range(len(nega_data))]
     for tab_cross in range(cross_folder):
@@ -67,6 +86,8 @@ def cross_tab(data,cross_folder,tab_cv):
 
 
         p_train = posi_data[p_index_train]
+        n_train = nega_data[n_index_train]
+
         p_test = posi_data[p_index_test]
         n_test = nega_data[n_index_test]
         #print(nega_data.shape)
@@ -74,12 +95,29 @@ def cross_tab(data,cross_folder,tab_cv):
         n_test2 = n_test[N - len(p_test) - 1:N - 1, :]
 
         test_data = np.append(p_test, n_test, axis=0)
+        testX = test_data[:,:-1]
+        testY = test_data[:,-1]
+
+        train_data = np.concatenate((n_train, p_train))
 
         #Random undersampling without replacement
-        #train_data = np.concatenate((nega_data[np.random.choice(n_index_train, len(p_train),replace=False)], p_train))
-        print()
-
-        return train_data[:,:-1],train_data[:,-1],test_data[:,:-1],test_data[:,-1]
+        if flag == 'RUS':
+            train_data = np.concatenate((nega_data[np.random.choice(n_index_train, len(p_train),replace=False)], p_train))
+            trainX = train_data[:,:-1]
+            trainY = train_data[:,-1]
+        elif flag == "SMOTE":
+            sm_ = SMOTE(kind='regular', verbose=True)
+            trainX, trainY = sm_.fit_transform(train_data[:,:-1], train_data[:,-1])
+        elif flag == "ROS":
+            os_ = OverSampler(verbose=True)
+            trainX, trainY = os_.fit_transform(train_data[:,:-1], train_data[:,-1])
+        elif flag == "NCL":
+            ncr_ = NeighbourhoodCleaningRule(verbose=True)
+            trainX, trainY = ncr_.fit_transform(train_data[:,:-1], train_data[:,-1])
+        elif flag == "USCC":
+            cc_ = ClusterCentroids(verbose=True)
+            trainX, trainY = cc_.fit_transform(train_data[:,:-1], train_data[:,-1])
+        return trainX,trainY,testX,testY
 """
 input_data_path = os.path.join(os.getcwd(),"BGPData")
 filename = "HB_Nimda.txt"
